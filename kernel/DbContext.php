02267baf8eb235a82c3b352f2016a9a142f39829
Hervé CREVAN <hcrevan@diginamic.fr>
2024-11-14 16:59:04 +0100

    add comments

--- kernel/DbContext.php
+++ kernel/DbContext.php
@@ -3,7 +3,13 @@
 namespace Keha\Kernel;
 
 use PDOException;
-
+/*
+* La classe a la responsabilité de se connecter à une BDD
+* Les informations de connexion sont dans la configuration(i.e: $_ENV)
+* Utilise le design pattern singleton pour renvoyer
+* toujours la même instance (donc la même connection à l'utilisateur)
+* Pour recupérer une connection, il faut passer par la méthode getConnexion
+*/
 class DbContext {
     private static ?\PDO $connexion = null;
 
@@ -17,6 +23,12 @@ class DbContext {
         
     }
 
+    /*
+    * méthode statique hors contexte objet 
+    * vérifie que la propriété est null
+    * si elle est null, elle instanie la classe pour créer une connexion PDO
+    * enfin, elle renvoie la propriété statique connexion qui est un objet PDO
+    */
     public static function getConnexion() {
         if (self::$connexion === null) {
             new static();
--- kernel/Repository.php
+++ kernel/Repository.php
@@ -3,6 +3,9 @@ namespace Keha\Kernel;
 
 use Keha\Kernel\DbContext;
 
+/*
+* Classe qui a la responsabilité de créer des requêtes vers la BDD
+*/
 class Repository {
     private \PDO $connexion;
     private string $model;
@@ -10,12 +13,20 @@ class Repository {
     private ?string $sql;
     private ?\PDOStatement $request;
 
+    /*
+    * Crée une connexion à la BDD via DbContext
+    * Prend un paramètre qui est le nom de la table à adresser
+    */
     public function __construct(string $model) {
         $this->connexion = DbContext::getConnexion();
         $this->table = $model;
+        // Se réfère à la classe sur laquelle mapper les résultats
         $this->model = $_ENV['model'].$model;
     }
 
+    /*
+    * Récupère un objet en fonction de l'id fournit en paramètre
+    */
     public function getById(int $id): array
     {
         $this->sql = 'select * from '.$this->table.' where id = ?';
@@ -23,6 +34,9 @@ class Repository {
         return $this->flush();
     }
 
+    /*
+    * Récupère un tableau d'objets depuis la BDD
+    */
     public function getAll(): array
     {
         $this->sql = 'select * from '.$this->table;
@@ -30,25 +44,42 @@ class Repository {
         return $this->flush();
     }
 
+    /*
+    * Pépare et exécute une requête
+    * le paramètre args est un tableau d'arguments pour la requête SQL
+    */
     public function prepare(?array $args = null): void
     {
         $this->request = $this->connexion->prepare($this->sql);
         $this->request->execute($args);
     }
 
+    /*
+    * Récupère un jeu de données (requête select) et 
+    * mappe les résultats sur la classe correspondante à la table
+    */
     public function flush(): array
     {
         return $this->request->fetchAll(\PDO::FETCH_CLASS, $this->model);
     }
 
-    public function insert($entity) {
+    /*
+    * Crée un nouvel enregistrement à partir d'un objet de type Model
+    * 
+    *
+    */
+    public function insert($entity):void {
+        //Récupère les noms des colonnes de la table qui correspond à l'entité
         $columns = $this->showColumn();
         $getter = [];
         $attributes = '';
         $count = count($columns)-1;
         foreach ( $columns as $key=>$column) {
+            //on crée dynamiquement le getter pour la proprité de la classe
             $getterFunction = 'get'.ucfirst($column);
+            // On inèsre dans le tableau getter la valeur pour la proprité de l'objet entity
             $getter[] = $entity->$getterFunction();
+            // On gère les attributs pour la requête préparée
             if ($key != $count) {
                 $attributes .= '? ,';
             } else {
@@ -56,12 +87,18 @@ class Repository {
             }
             
         }
-        var_dump($getter);
+        // On écrit la requête SQL
         $this->sql = 'insert into '.$this->table.' values ('.$attributes.')';
+        // On prépare et exécute la requête d'insertion
         $this->prepare($getter);
     }
 
-    private function showColumn()
+
+    /*
+    * Récupère les informations des colonnes
+    * pour une table
+    */
+    private function showColumn(): array
     {
         $this->sql = 'SHOW COLUMNS FROM '.$this->table;
         $this->prepare();
